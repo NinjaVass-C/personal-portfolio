1a752ec89885b6f0d6491bc2caa48701b76a1f0d
NinjaVass-C <connorvassinfo@gmail.com>
2026-03-16 20:14:45 -0400

    Styling adjustments across existing components to match newly created about me page

---
+++ src/components/AboutCard.tsx
@@ -0,0 +1,14 @@
+export function AboutCard({inputText, inputTitle} : {inputText: string, inputTitle: string}) {
+
+
+    return (
+        <div className={'w-1/4 bg-blue-300/30 justify-center mx-auto rounded-2xl p-4'}>
+            <h2 className='text-2xl font-bold justify-center flex text-gray-200'>
+                {inputTitle}
+            </h2>
+            <p className={'flex text-l items-center text-gray-50 text-center'}>
+                {inputText}
+            </p>
+        </div>
+    )
+}
