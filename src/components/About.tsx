6a3b186ea9efda608ed9d5a763b5309f5219236d
NinjaVass-C <connorvassinfo@gmail.com>
2026-03-18 17:19:30 -0400

    Added contact section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 export function About() {
     return (
         <div className='max-w-4xl mx-auto text-center px-6'>
-            <p className={'text-2xl items-center p-12 text-gray-50 text-center border-2 border-blue-300/60 bg-blue-300/10 rounded-2xl'}>
+            <p className={'text-2xl p-12 text-gray-50 text-center border-2 border-blue-300/60 bg-blue-300/10 rounded-2xl'}>
                 Hello, my name is Connor Vass, and I am a Computer Programming and Analysis student set to graduate this year.
                 I also work part time as a Student Software Engineer at <a className={'text-blue-200'} href={'https://elentra.com/'}>Elentra</a>, modernizing and maintaining a legacy codebase.
                 I started my software development journey while I was in high school, and hope to continue learning and honing my
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,6 +1,6 @@
 export function Introduction() {
     return (
-        <div id='introduction' className='flex items-center p-20 rounded-sm flex-col h-full grow justify-center gap-8'>
+        <div className='flex items-center p-20 rounded-sm  h-full grow justify-center gap-8'>
             <div className='flex text-2xl flex-col items-center p-12 text-gray-50 rounded-2xl border-2 border-blue-300/60 bg-blue-300/10'>
                 <h2 className='text-6xl font-bold mb-4'>Connor Vass</h2>
                 <p>I am a third year Computer Programming and Analysis Student,</p>
